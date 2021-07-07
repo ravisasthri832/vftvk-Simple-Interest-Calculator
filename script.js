96b3598a2a6a25a850f9f64e8c0db5d188b19e11
Ravikumar Pendyala <ravisasthri832@gmail.com>
2021-07-07 15:16:00 +0530

    few changes updated

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-function compute() {
-
+function compute() 
+{
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
 
-    if (principal == '') {
-        alert('enter number');
+    if (principal == '' || principal <= 0) {
+        alert('Enter a positive number');
         principal.focus();
         return false
     } 
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear() + parseInt(years);
 
-    document.getElementById("result").innerHTML = "If you deposite " + principal + "<br/>at an interest rate of " + rate + "<br/> You will receive an amount of " + interest + "<br/>in the year " + year
+    document.getElementById("result").innerHTML = "If you deposit " + "<span class='bgyello'>" + principal + "</span>"  + ", <br> at an interest rate of "+ "<span class='bgyello'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='bgyello'>" + interest + "</span>" + ", <br> in the year " + "<span class='bgyello'>" + year + "</span>.";
 }
 
-function updateRate() {
-    var r = document.getElementById("rate").value;
-    document.getElementById('rate_value').innerHTML = r;
+function updateROI() {
+    var rate = document.getElementById("rate").value;
+    document.getElementById('rate_value').innerHTML=rate;
+	output.innerHTML = rate.value;
+	slider.oninput = function() 
+    {
+        output.innerHTML = this.value;
+    }
 }
